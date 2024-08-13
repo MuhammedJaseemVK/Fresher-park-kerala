83bb8f8a6a6200b7ca8a6e5b58d60f2096a25199
Jaseem <muhammedjaseemvk96@gmail.com>
2024-08-13 22:35:07 +0530

    fixed wrong path to frontend

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,10 @@ cron.schedule('0 * * * *', async () => {
      scrapJobsController();
 });
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 );
 
 
