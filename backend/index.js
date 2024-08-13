8b3cdd7819a29231e4eb039808481b505cd49179
Jaseem <muhammedjaseemvk96@gmail.com>
2024-08-13 21:25:49 +0530

    corrected wrong es5 syntax

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,10 @@ cron.schedule('0 * * * *', async () => {
      scrapJobsController();
 });
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
 
