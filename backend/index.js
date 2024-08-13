7e4635db0489c15a7084b06547ea6532640e3a07
Jaseem <muhammedjaseemvk96@gmail.com>
2024-08-13 22:26:09 +0530

    added missing import

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const cron = require('node-cron');
 
