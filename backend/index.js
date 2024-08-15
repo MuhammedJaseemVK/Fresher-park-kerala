1d92e251f9e0ee071e2ae64cf94d18ed6bbc1409
Jaseem <muhammedjaseemvk96@gmail.com>
2024-08-15 12:40:47 +0530

    adding ping request to keep server alive

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require('./config/db');
 const cron = require('node-cron');
+const https =require('https');
 
 const port = 8080;
 const jobRoutes = require('./routes/jobRoutes');
@@ -14,9 +15,39 @@ dotenv.config();
 connectDB();
 app.use(cors());
 app.use('/api/v1/jobs', jobRoutes);
-cron.schedule('0 * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
      scrapJobsController();
 });
+
+
+function pingServer() {
+    console.log('Pinging server to keep it alive...');
+    
+    const options = {
+        hostname:'https://fresher-park-kerala.onrender.com/',
+        method: 'GET',
+        timeout: 60000 
+    };
+
+    const req = https.request(options, (res) => {
+        console.log(`Ping response: ${res.statusCode}`);
+
+    });
+
+    req.on('timeout', () => {
+        req.destroy();
+        console.error('Request timed out');
+    });
+
+    req.on('error', (err) => {
+        console.error('Ping error:', err.message);
+    });
+
+    req.end();
+}
+
+cron.schedule('*/10 * * * *', pingServer);
+
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
